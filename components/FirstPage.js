3971d5b8d9ec3f5d8319b8152c675532f8706be6
jaoharil <jaoharilmaknun2904@gmail.com>
2024-12-01 15:01:19 +0800

    cek error

--- components/FirstPage.js
+++ components/FirstPage.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 import Img from '../../assets/img2.png';
 
 const FirstPage = () => {
   return (
     <div className="h-full">
-      <main className="w-full h-full bg-secondary page-shadow ">
+      <main className="w-full h-full bg-secondary page-shadow">
         <div className="px-10 space-y-4">
           {/* image container */}
           <div>
-            <img src={Img} alt="" className="w-[200px] h-[190px] mx-auto  " />
+            <Image src={Img} alt="Description of the image" width={200} height={190} className="mx-auto" />
           </div>
           {/* text container */}
           <div>
-            <div className="">
+            <div>
               <h1 className="uppercase">Hello</h1>
               <p className="text-3xl md:text-5xl font-bold">Im Jaoharil</p>
-              <p className="text-left ">Web Developer</p>
+              <p className="text-left">Web Developer</p>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cumque voluptas mollitia ducimus rem</p>
-              {/* <a href="" className="inline-block bg-primary text-white cursor-pointer hover:bg-primaru/80 py-2 px-4 my-3 rounded-lg">
-              Her Me
+              {/* <a href="" className="inline-block bg-primary text-white cursor-pointer hover:bg-primary/80 py-2 px-4 my-3 rounded-lg">
+              Hire Me
             </a> */}
             </div>
           </div>
--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { HiHome, HiUser, HiViewColumns, HiRectangleGroup, HiChatBubbleBottomCenterText, HiEnvelope } from 'react-icons/hi2';
+import { HiHome, HiUser, HiViewColumns, HiRectangleGroup, HiEnvelope } from 'react-icons/hi2';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -8,7 +8,6 @@ export const navData = [
   { name: 'about', path: '/about', icon: <HiUser /> },
   { name: 'certificate', path: '/services', icon: <HiRectangleGroup /> },
   { name: 'work', path: '/work', icon: <HiViewColumns /> },
-  // { name: 'testimonials', path: '/testimonials', icon: <HiChatBubbleBottomCenterText /> },
   { name: 'contact', path: '/contact', icon: <HiEnvelope /> },
 ];
 
@@ -17,49 +16,34 @@ const Nav = () => {
   const pathname = router.pathname;
   const [activeTooltip, setActiveTooltip] = useState(null);
 
-  const handleMouseEnter = (index) => {
-    setActiveTooltip(index); // Menampilkan tooltip saat hover
-  };
-
-  const handleMouseLeave = () => {
-    setActiveTooltip(null); // Menyembunyikan tooltip setelah hover
-  };
-
-  const handleClick = () => {
-    setActiveTooltip(null); // Menyembunyikan tooltip saat diklik
-  };
+  const handleMouseEnter = (index) => setActiveTooltip(index);
+  const handleMouseLeave = () => setActiveTooltip(null);
+  const handleClick = () => setActiveTooltip(null);
 
   return (
     <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-[80px] bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
       <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
-        {navData.map((link, index) => (
-          <div key={index} className="relative flex items-center">
+        {navData.map((link) => (
+          <div key={link.name} className="relative flex items-center">
             <Link
               className={`${link.path === pathname && 'text-kuning'} flex items-center group hover:text-accent transition-all duration-300`}
               href={link.path}
               onClick={handleClick}
-              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseEnter={() => handleMouseEnter(link.name)}
               onMouseLeave={handleMouseLeave}
+              aria-label={link.name}
             >
-              {/* icons */}
-              <div>{link.icon}</div>
+              <div className="text-2xl">{link.icon}</div>
             </Link>
 
-            {/* Tooltip untuk layar besar (ke kiri) */}
-            {activeTooltip === index && (
-              <div className="hidden xl:block absolute right-full mr-2 bg-white p-[6px] rounded-[3px] text-primary text-center">
+            {activeTooltip === link.name && (
+              <div
+                className={`absolute ${activeTooltip === link.name ? 'block' : 'hidden'} xl:right-full xl:mr-2 xl:bg-white xl:p-[6px] xl:rounded-[3px] xl:text-primary xl:text-center xl:top-1/2 xl:-translate-y-1/2 
+                block xl:hidden bottom-full left-1/2 transform -translate-x-1/2 mb-2 bg-white p-[6px] rounded-[3px] text-primary text-center`}
+              >
                 <div className="text-sm font-semibold capitalize">{link.name}</div>
-                {/* Triangle */}
-                <div className="absolute right-[-6px] top-1/2 transform -translate-y-1/2 w-0 h-0 border-t-4 border-b-4 border-l-4 border-t-transparent border-b-transparent border-l-white"></div>
-              </div>
-            )}
-
-            {/* Tooltip untuk layar HP (di atas) */}
-            {activeTooltip === index && (
-              <div className="block xl:hidden absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 bg-white p-[6px] rounded-[3px] text-primary text-center">
-                <div className="text-sm font-semibold capitalize">{link.name}</div>
-                {/* Triangle */}
-                <div className="absolute bottom-[-6px] left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-white"></div>
+                <div className="absolute bottom-[-6px] left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-white xl:hidden"></div>
+                <div className="absolute right-[-6px] top-1/2 transform -translate-y-1/2 w-0 h-0 border-t-4 border-b-4 border-l-4 border-t-transparent border-b-transparent border-l-white hidden xl:block"></div>
               </div>
             )}
           </div>
